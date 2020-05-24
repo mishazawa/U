df91b215d9755eed0800265f3b5f0ff4973ff476
mishazawa <macintoshkrug@gmail.com>
2020-05-25 02:12:10 +0300

    initial commit

---
+++ Assets/Scripts/FpsMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FpsMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float gravity = -9.8f;
+
+
+    private CharacterController characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dx = Input.GetAxis("Horizontal") * speed;
+        var dz = Input.GetAxis("Vertical") * speed;
+
+        var movement = new Vector3(dx, 0, dz);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        movement *= Time.deltaTime;
+
+        movement = transform.TransformDirection(movement);
+        characterController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public enum Axes {
+        MouseXY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public Axes axes = Axes.MouseXY;
+    public float sensivityHor = 5f;
+    public float sensivityVer = 3f;
+
+    public float limitRotationVer = 45f;
+
+    // -------
+
+    private float rotationVer = 0f;
+
+    void Start()
+    {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null) {
+            body.freezeRotation = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == Axes.MouseX) {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensivityHor, 0);
+        }
+
+        if (axes == Axes.MouseY) {
+            rotationVer -= Input.GetAxis("Mouse Y") * sensivityVer;
+            rotationVer =  Mathf.Clamp(rotationVer, -limitRotationVer, limitRotationVer);
+            transform.localEulerAngles = new Vector3(rotationVer, transform.localEulerAngles.y, 0);
+        }
+    }
+}
