b81f2ba66d8c740f469bdffb544e9b73be6dc241
mishazawa <macintoshkrug@gmail.com>
2020-06-04 01:17:19 +0300

    CH 8 relative camera

---
+++ Assets/Scripts/OrbitCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector3 offset;
+    private float rotationY;
+    public float rotationSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = target.position - gameObject.transform.position;
+        rotationY = gameObject.transform.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        rotationY += Input.GetAxis("Mouse X") * rotationSpeed;
+        Quaternion rotation = Quaternion.Euler(0, rotationY, 0);
+        gameObject.transform.position = target.position - rotation * offset;
+        gameObject.transform.LookAt(target);
+    }
+}
---
+++ Assets/Scripts/RelativeMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class RelativeMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private CharacterController controller;
+    public float rotationSpeed = 15f;
+    public float moveSpeed = 6f;
+
+    private void Start() {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        var hor = Input.GetAxis("Horizontal");
+        var ver = Input.GetAxis("Vertical");
+
+        if (hor != 0 || ver != 0) {
+            var movement = Vector3.ClampMagnitude(new Vector3(hor * moveSpeed, 0, ver * moveSpeed), moveSpeed);
+            var tmp = target.rotation;
+
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+
+            var direction = Quaternion.LookRotation(movement);
+            gameObject.transform.rotation = Quaternion.Lerp(
+                gameObject.transform.rotation,
+                direction,
+                rotationSpeed * Time.deltaTime
+            );
+
+            movement *= Time.deltaTime;
+            controller.Move(movement);
+        }
+    }
+}
